10082f765b51d9822d95d8cecaf78ba38b09c639
Shabbir Ahmed <samax.mw@gmail.com>
2019-05-24 05:54:44 +0600

    minor fix for realeas build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
         if (!clientid || clientid.trim().length === 0) return;
         else UserKit.localuser = clientid = clientid.trim();
 
-        var serverurl = "ws://52.221.192.102:8083";//"ws://qmixin.com:8083/";
+        var serverurl = "wss://bu-connect.qmixin.com:8083";//"ws://qmixin.com:8083/";
         signalx.socket = new WebSocket(serverurl + "/" + clientid);
 
         signalx.init();
